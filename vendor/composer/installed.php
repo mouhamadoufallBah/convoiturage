1feeb39b85e42bfcdb16a5da3f2745a9ea40005f
mouhamadoufallbah <mouhamadoufall.bah@gmail.com>
2023-04-08 04:51:03 +0000

    Partie security

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'convoiturage/convoiturage',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '437eecab67924ac87637599b842ea4451b97d533',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'convoiturage/convoiturage' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '437eecab67924ac87637599b842ea4451b97d533',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.2.3',
-            'version' => '5.2.3.0',
-            'reference' => 'cb021439c683d9805e2864c58095b92d405e9b11',
+            'pretty_version' => 'v5.3.0-alpha3',
+            'version' => '5.3.0.0-alpha3',
+            'reference' => 'ac576614a5515e429f27e756fad81d5aa05e95a6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../twbs/bootstrap',
             'aliases' => array(),
@@ -31,7 +31,7 @@
         'twitter/bootstrap' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => 'v5.2.3',
+                0 => 'v5.3.0-alpha3',
             ),
         ),
     ),
